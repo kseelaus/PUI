dd56853e7ecbb3f36b27a268c0c2c9442a365b65
kseelaus <43346943+kseelaus@users.noreply.github.com>
2018-10-01 12:03:41 -0400

    week 6 in-lab!

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -25,3 +25,65 @@ function completeListItem(item) {
     }
 }
 
+/*$(document).ready(function(){
+    $("#add-item").click(function() { // bind handler for click event
+        var list = $("#grocery-list"); // get the ol list by id
+        var itemInput = $("#new-list-item"); // get the new item input
+	  // append the input value within an li element
+        list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>") 
+    });
+    $(".delete-item").click(function() {
+        $(this).parent().remove();
+        console.log("test");
+    });
+    
+    
+});*/
+
+$(document).on("click", ".delete-item", function() {
+    $(this).parent().remove();
+});
+$(document).on("click", "#add-item", function() {
+    var list = $("#grocery-list");
+    var itemInput = $("#new-list-item");
+    list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+});
+
+
+$(document).on("click", ".complete-item", function(){
+    if($(this).is(':checked')) {
+        $(this).parent().css( "textDecoration", "line-through" );
+    } else {
+        $(this).parent().css( "textDecoration", "none" );
+    }
+    console.log("test");
+});
+
+
+
+/*
+$(document).on("click", ".complete-item", function(){
+    var isFormParent = $( "input[type='checkbox']" ).parent().is( "form" );
+    if isFormParent === true {
+        $(this).css( "textDecoration", "line-through" );
+    } else {
+        $(this).css( "textDecoration", "none" );
+    }
+    console.log("test");
+});*/
+
+/*$(".complete-item").click(function(event){
+    var target = $( event.target );
+    if ( target.is( "checked" ) ) {
+        target.css( "textDecoration", "line-through" );
+    } else {
+        target.css( "textDecoration", "none" );
+    }
+    console.log("test");
+});*/
+
+
+
+
+
+
